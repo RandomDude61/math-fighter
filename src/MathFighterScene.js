7378d53a7342e83a7522a5fa24f367ae223dfc85
RandomDude61 <150793210+RandomDude61@users.noreply.github.com>
2023-11-23 17:39:00 +0700

    Meeting 17 (Add Number)

--- src/MathFighterScene.js
+++ src/MathFighterScene.js
@@ -4,15 +4,25 @@ export default class MathFighterScene extends Phaser.Scene {
 	constructor() {
 		super('math-fighter-scene')
 	}
+	// INIT METHOD
 	init() {
+		// PREPARE GAME SCREEN
 		this.gameHalfWidth = this.scale.width * 0.5
 		this.gameHalfHeight = this.scale.height * 0.5
+
+		// CREATE SPIRTE
 		this.player = undefined
 		this.enemy = undefined
 		this.slash = undefined
+
+		// START BUTTON
 		this.startGame = false
+
+		// SHOWING TEXT
 		this.questionText = undefined
 		this.resultText = undefined
+
+		// CREATE BUTTONS
 		this.button1 = undefined
 		this.button2 = undefined
 		this.button3 = undefined
@@ -25,6 +35,10 @@ export default class MathFighterScene extends Phaser.Scene {
 		this.button0 = undefined
 		this.buttonDel = undefined
 		this.buttonOk = undefined
+
+		// GIVING VALUE NUMBERS
+		this.numberArray = []
+		this.number = 0
 	}
 
 	// PRELOAD METHOD
@@ -84,7 +98,7 @@ export default class MathFighterScene extends Phaser.Scene {
 		
 		start_button.on('pointerdown', () => {
 			this.gameStart()
-			start_button.destroy()
+			start_button.destroy();
 		}, this)
 	}
 
@@ -93,8 +107,10 @@ export default class MathFighterScene extends Phaser.Scene {
 
 	}
 
-	// CREATE ANOMATION METHOD
+	// CREATE ANIMATION METHOD
 	createAnimation() {
+
+		// PLAYER ANIMATIONS
 		this.anims.create({
 			key: `player-standby`,
 			frames: this.anims.generateFrameNumbers('player',
@@ -120,6 +136,8 @@ export default class MathFighterScene extends Phaser.Scene {
 					{start: 0, end: 4}),
 			frameRate: 10,
 		})
+
+		// ENEMY ANIMATIONS
 		this.anims.create({
 			key: `enemy-standby`,
 			frames: this.anims.generateFrameNumbers('enemy',
@@ -153,8 +171,10 @@ export default class MathFighterScene extends Phaser.Scene {
 		this.player.anims.play('player-standby', true)
 		this.enemy.anims.play('enemy-standby', true)
 		this.resultText = this.add.text(this.gameHalfWidth,200,
+		// @ts-ignore
 		'0', { fontSize : '32px', fill: '#000'})
 		this.questionText = this.add.text(this.gameHalfWidth,100,
+		// @ts-ignore
 		'0', { fontSize : '32px', fill: '#000'})
 		
 		this.createButtons()
@@ -163,21 +183,84 @@ export default class MathFighterScene extends Phaser.Scene {
 
 	// CREATE BUTTON METHOD
 	createButtons() {
-		const startPosY = this.scale.height = 246
-		const wdithDiff = 131
+		const startPosY = this.scale.height - 246
+		const widthDiff = 131
 		const heightDiff = 71.25
 
-		this.button2 = this.add.image(this.gameHalfHeight,startPosY,
+		// CENTER BUTTONS
+		this.button2 = this.add.image(this.gameHalfWidth,startPosY,
 		'numbers', 1).setInteractive().setData('value',2)
 
 		this.button5 = this.add.image(this.gameHalfWidth,this.button2.y
 		+ heightDiff, 'numbers', 4).setInteractive().setData('value', 5)
 
-		this.button8 = this.add.image(this.gameHalfWidth,this.button2.y
+		this.button8 = this.add.image(this.gameHalfWidth,this.button5.y
 		+ heightDiff, 'numbers', 7).setInteractive().setData('value', 8)
 
-		this.button0 = this.add.image(this.gameHalfWidth,this.button2.y
+		this.button0 = this.add.image(this.gameHalfWidth,this.button8.y
 			+ heightDiff, 'numbers', 10).setInteractive().setData('value', 0)
+
+		// LEFT BUTTONS
+		this.button1 = this.add.image(this.button2.x - widthDiff, startPosY,
+		'numbers', 0).setInteractive().setData('value',1)
+		
+		this.button4 = this.add.image(this.button5.x - widthDiff,
+		this.button1.y + heightDiff, 'numbers', 3).setInteractive()
+		.setData('value', 4)
+		
+		this.button7 = this.add.image(this.button8.x - widthDiff,
+		this.button4.y + heightDiff, 'numbers', 6).setInteractive()
+		.setData('value', 7)
+		
+		this.buttonDel = this.add.image(this.button0.x - widthDiff,
+		this.button7.y + heightDiff, 'numbers', 9).setInteractive()
+		.setData('value', 'del')
+
+		// RIGHT BUTTONS
+		this.button3 = this.add.image(this.button2.x + widthDiff, startPosY,
+			'numbers', 2).setInteractive().setData('value', 3)
+			
+		this.button6 = this.add.image(this.button5.x + widthDiff,
+		this.button3.y + heightDiff, 'numbers', 5).setInteractive()
+			.setData('value', 6)
+			
+		this.button9 = this.add.image(this.button8.x + widthDiff,
+		this.button6.y + heightDiff, 'numbers', 8).setInteractive()
+			.setData('value', 9)
+			
+		this.buttonOk = this.add.image(this.button0.x + widthDiff,
+		this.button9.y + heightDiff, 'numbers', 11).setInteractive()
+			.setData('value', 'ok')
 	}
+	// ADD NUMBER METHOD
+	addNumber(pointer, object, event){
+
+		let value = object.getData('value')
+
+		if (isNaN(value)) {
+			if(value == 'del') {
+				this.numberArray.pop()
+				if(this.numberArray.length < 1) {
+					this.numberArray[0] = 0
+				}
+			}
+			if(value == 'ok') {
+					this.checkAnswer()
+				 	this.numberArray = []
+					this.numberArray[0] = 0
+			}
+		}
+		else {
+			if (this.numberArray.length==1 && this.numberArray[0]==0){
+				this.numberArray[0] = value
+			} else {
+				if (this.numberArray.length < 10){
+					this.numberArray.push(value)
+				}
+			}
+		}
+		
+		this.number = parseInt(this.numberArray.join(''))
 
 }
+	}
